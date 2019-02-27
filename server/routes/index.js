701d81fb429c60ce67b02ce49b5502dc5799d7a4
Liam Dyer <saghendev@gmail.com>
2019-02-27 15:52:45 -0500

    initialize db in routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,6 @@
-module.exports = function(Koa) {
+module.exports = async function(Koa) {
+  const db = require('Core/mongo')();
+
   Koa.use(require('./api/blog').middleware());
   Koa.use(require('./api/auth').middleware());
 }
