183dfb76e2c237e94dc6fb763b6269cd22f971b1
Liam Dyer <saghendev@gmail.com>
2019-02-27 15:52:58 -0500

    signup and login route wips

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -3,25 +3,79 @@ const Joi = router.Joi;
 
 const auth = router();
 
-auth.prefix('/api/auth')
+const User = require('Models/user');
+const db = require('Core/mongo');
+
+const errorHelper = require('Core/errorHelper');
+const HttpStatus = require('http-status-codes');
+const RateLimit = require('koa2-ratelimit').RateLimit;
+const crypto = require('crypto');
+
+auth.prefix('/api/auth');
+
+auth.route({
+  method: 'post',
+  path: '/signup',
+  validate: {
+    type: 'form',
+    body: {
+      firstName: Joi.string().trim().min(1).required(),
+      lastName: Joi.string().trim().min(1).required(),
+      email: Joi.string().trim().email().required(),
+      password: Joi.string().min(8).regex(/(.*[a-z].*)/).regex(/(.*[A-Z].*)/).regex(/(.*\d.*)/).required(),
+      profileImage: Joi.string().trim().lowercase()
+    },
+    continueOnError: true
+  },
+  handler: async ctx => {
+    if (!await errorHelper.verifyRoute(ctx)) return;
+
+    let user = new User(ctx.request.body);
+
+    if (await User.findOne({ email: user.email })) {
+      ctx.status = HttpStatus.CONFLICT;
+      ctx.body = errorHelper.build({ keys: 'email', status: HttpStatus.CONFLICT, description: 'A user with that email already exists' });
+      return;
+    }
+
+    await user.save();
+    // send an email with the verification token 'verifyToken'
+
+    ctx.body = {
+      success: true,
+      message: 'Successfully created account'
+    }
+  }
+})
 
 auth.route({
   method: 'post',
   path: '/login',
   validate: {
-
+    type: 'form',
+    body: {
+      email: Joi.string().trim().email(),
+      password: Joi.string().min(8).regex(/(.*[a-z].*)/).regex(/(.*[A-Z].*)/).regex(/(.*\d.*)/)
+    }
   },
+  pre: errorHelper.middleware,
   handler: async ctx => {
-    await checkAuth(ctx);
+    const user = await User.findOne({ email: ctx.request.body.email });
+
+    if (!user) return;
+
+    if (!user.comparePassword(user.password)) return;
+
+    const token = crypto.randomBytes(64).toString('hex');
+    console.log(token);
   }
 })
 
 auth.get('/login', async ctx => {
-  ctx.body = "Yo";
-})
-
-auth.get('/signup', async ctx => {
-  ctx.body = "Yo";
+  ctx.body = {
+    success: true,
+    message: 'Successfully created account'
+  }
 })
 
 auth.get('/resetPassword', async ctx => {
@@ -33,3 +87,6 @@ auth.get('/changePassword', async ctx => {
 })
 
 module.exports = auth;
+
+// postedBy: Joi.string().regex(/^[a-f\d]{24}$/i).required(),
+// title: Joi.string().invalid()
