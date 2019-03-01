86a9daed46ed467e7449effef99237b853492438
Liam Dyer <saghendev@gmail.com>
2019-03-01 08:57:05 -0500

    Blog router wip

--- server/routes/api/blog.js
+++ server/routes/api/blog.js
@@ -1,12 +1,55 @@
 const router = require('koa-joi-router');
 const Joi = router.Joi;
 
+const C = require('../../core/constants');
+const Post = require('../../models/post');
+
 const blog = router();
 
 blog.prefix('/api/blog')
 
-blog.get('/', ctx => {
-  ctx.body = "Yo";
+blog.route({
+  method: 'get',
+  path: '/list',
+  validate: {
+    query: {
+      topic: Joi.string().only(...C.topics),
+      page: Joi.number().default(0),
+      pageCount: Joi.number().default(10)
+    },
+  },
+  async handler(ctx) {
+    let query = {
+      id: { $gt: ctx.query.page * ctx.query.pageCount, $lt: ctx.query.page * ctx.query.pageCount + ctx.query.pageCount - 1 }
+    }
+
+    if (ctx.query.topic) {
+      query.topic = ctx.query.topic;
+    }
+
+    let posts = await Post.find(query);
+
+    ctx.body = posts;
+  }
+})
+
+blog.route({
+  method: 'get',
+  path: '/list',
+  validate: {
+    query: {
+      id: Joi.number().required()
+    }
+  },
+  async handler(ctx) {
+    let query = {
+      id: ctx.query.id
+    }
+
+    let post = await Post.findOne(query);
+
+    ctx.body = post;
+  }
 })
 
 module.exports = blog;
