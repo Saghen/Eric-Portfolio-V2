dcd7769e71aa3aad019f8a71c01b5fad320df2ac
Liam Dyer <saghendev@gmail.com>
2019-03-03 16:44:53 -0500

    Api logging

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,12 @@
+const logger = require('Core/logger');
+const apiLogger = require('koa-logger');
+
 module.exports = async function(Koa) {
   const db = require('Core/mongo')();
+
+  Koa.use(apiLogger((str, args) => {
+    logger.debug(str);
+  }))
 
   Koa.use(require('./api/blog').middleware());
   Koa.use(require('./api/auth').middleware());
