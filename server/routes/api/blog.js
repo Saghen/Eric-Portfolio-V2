546f1dea3dd99077863e2aa34758a5f8d18be929
Liam Dyer <saghendev@gmail.com>
2019-03-23 16:16:13 -0400

    Case-insensitivity for blog posts, better error handling and responses, global find functions, delete endpoint

--- server/routes/api/blog.js
+++ server/routes/api/blog.js
@@ -1,3 +1,7 @@
+'use strict'
+
+const config = require('Config/');
+
 const router = require('koa-joi-router');
 const Joi = router.Joi;
 
@@ -8,7 +12,11 @@ const reqHelper = require('Helpers/reqHelper');
 const HttpStatus = require('http-status-codes');
 const RateLimit = require('koa2-ratelimit').RateLimit;
 
+const koaJwt = require('koa-jwt');
+
 const blog = router();
+
+const mongoose = require('mongoose');
 
 blog.prefix('/api/blog');
 
@@ -28,16 +36,28 @@ blog.route({
       image: Joi.string().required(),
       content: Joi.string().min(8).required(),
       topic: Joi.string().trim().only(...C.topics),
-      date: Joi.date()
+      date: Joi.date(),
+      postedBy: Joi.string().regex(/^[a-f\d]{24}$/i).required()
     },
+    maxBody: Number.POSITIVE_INFINITY,
     continueOnError: true
   },
+  pre: koaJwt({ secret: config.jwt.secret, cookie: 'token' }),
   handler: async ctx => {
     if (!(await reqHelper.verifyRoute(ctx))) return;
 
+    ctx.request.body.postedBy = mongoose.Types.ObjectId(
+      ctx.request.body.postedBy
+    );
+
     let post = new Post(ctx.request.body);
 
-    if (await Post.findOne({ title: post.title })) {
+    if (
+      await Post.findOne({ title: post.title }).collation({
+        locale: 'en',
+        strength: 2
+      })
+    ) {
       ctx.status = HttpStatus.CONFLICT;
       ctx.body = reqHelper.buildError({
         errors: [
@@ -51,7 +71,7 @@ blog.route({
     try {
       await post.save();
     } catch (err) {
-      console.error(err.message);
+      logger.error(err.message);
     }
 
     ctx.body = reqHelper.build({ message: 'Successfully created blog post' });
@@ -64,22 +84,14 @@ blog.route({
   validate: {
     query: {
       maxitems: Joi.number().default(20),
-      count: Joi.number().default(0)
+      page: Joi.number().default(1)
     },
     continueOnError: true
   },
   handler: async ctx => {
     if (!(await reqHelper.verifyRoute(ctx))) return;
 
-    let posts;
-
-    try {
-      posts = await Post.find({ hidden: false })
-        .skip(ctx.query.count)
-        .limit(ctx.query.maxItems);
-    } catch (err) {
-      console.error(err);
-    }
+    let posts = await findPosts(ctx.query);
 
     ctx.body = reqHelper.build({ data: posts });
   }
@@ -98,21 +110,49 @@ blog.route({
     continueOnError: true
   },
   handler: async ctx => {
+    console.log(ctx.cookies.get('token'));
     if (!(await reqHelper.verifyRoute(ctx))) return;
 
-    let post;
+    let post = await findOnePost(ctx.query);
 
-    try {
-      if (ctx.query.id)
-        post = await Post.findOne({ _id: ctx.query.id, hidden: false });
-      else if (ctx.query.title)
-        post = await Post.findOne({
-          title: ctx.query.title,
-          hidden: false
-        }).collation({ locale: 'en', strength: 2 }); // Makes the query case insensitive
-    } catch (err) {
-      console.error(err);
+    if (post === undefined || post === null) {
+      ctx.status = HttpStatus.NOT_FOUND;
+      ctx.body = reqHelper.buildError({
+        status: HttpStatus.NOT_FOUND,
+        errors: [
+          {
+            key: ctx.query.id ? 'id' : 'title',
+            message: 'The request blog post was not found'
+          }
+        ]
+      });
+      return;
     }
+
+    post.views++;
+
+    ctx.body = reqHelper.build({ data: post });
+
+    post.save();
+  }
+});
+
+blog.route({
+  method: 'delete',
+  path: '/delete',
+  validate: {
+    query: Joi.object()
+      .keys({
+        id: Joi.number().min(1),
+        title: Joi.string().min(1)
+      })
+      .xor('id', 'title'),
+    continueOnError: true
+  },
+  handler: async ctx => {
+    if (!(await reqHelper.verifyRoute(ctx))) return;
+
+    let post = await findOnePost(ctx.query);
 
     if (post === undefined || post === null) {
       ctx.status = HttpStatus.NOT_FOUND;
@@ -125,8 +165,35 @@ blog.route({
       return;
     }
 
-    ctx.body = reqHelper.build({ data: post });
+    post.remove();
   }
 });
+
+async function findOnePost({ id, title }) {
+  let post;
+
+  if (id)
+    post = await Post.findOne({
+      _id: id,
+      hidden: false
+    }).populate('postedBy');
+  else if (title)
+    post = await Post.findOne({
+      title: title,
+      hidden: false
+    })
+      .collation({ locale: 'en', strength: 2 })
+      .populate('postedBy', ['fullname', 'lastName', 'firstName', 'roles', 'profileImage']); // Makes the query case insensitive
+
+  return post;
+}
+
+async function findPosts({ maxitems, page }) {
+  return await Post.find({ hidden: false })
+    .sort({ _id: -1 })
+    .skip((page - 1) * maxitems)
+    .limit(maxitems)
+    .populate('postedBy', ['fullname', 'lastName', 'firstName', 'roles', 'profileImage']);
+}
 
 module.exports = blog;
