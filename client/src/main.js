0c1ff3e5fabb0dfe5a63a84c16ca09fd565494f8
Liam Dyer <saghendev@gmail.com>
2019-03-23 16:10:24 -0400

    Add axios, store, and router

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from './axios';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
+
+import store from './store';
+import router from './router';
 
 new Vue({
   router,
+  store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
