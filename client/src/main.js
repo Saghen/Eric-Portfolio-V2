885a7a1ae1950b57328430543b7ab98905091189
Liam Dyer <saghendev@gmail.com>
2019-04-10 14:31:10 -0400

    Modal, Toast, Capitalize prototype

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
-import App from './App.vue';
+import store from './store';
+import router from './router';
+
 import axios from './axios';
+import App from './App.vue';
+
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
 
-import store from './store';
-import router from './router';
+import VModal from 'vue-js-modal'
+
+Vue.use(VModal)
+
+import Toasted from 'vue-toasted';
+
+Vue.use(Toasted)
 
 new Vue({
-  router,
   store,
+  router,
   render: h => h(App)
 }).$mount('#app');
