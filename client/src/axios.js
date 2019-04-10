98e0f2c564d53c7f578ebc5311635ea7ff5402b0
Liam Dyer <saghendev@gmail.com>
2019-04-10 14:27:07 -0400

    Remove unneeded Axios configs

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-const accessToken = localStorage.getItem('access_token');
-
 export default axios.create({
-  baseURL: 'https://saghen.com/',
-  headers: {
-    Authorization: accessToken
-  }
+  baseURL: '',
 });
