3b6ce8594710f5ae883e426fe4f1e43ae7cd9191
Liam Dyer <saghendev@gmail.com>
2019-04-10 14:20:49 -0400

    Config - JWT, images, isDev/Prod

--- server/config/index.js
+++ server/config/index.js
@@ -4,6 +4,8 @@ let path = require('path');
 
 global.rootPath = path.join(__dirname);
 
+const mode = process.env.NODE_ENV || 'development';
+
 module.exports = {
   app: {
     url: 'https://saghen.com/',
@@ -17,18 +19,30 @@ module.exports = {
     https: process.env.HTTPS_PORT || 443,
   },
 
-  mode: process.env.NODE_ENV || 'development',
+  mode,
 
-  rootPath: global.rootPath,
+  env: {
+    isDevelopment,
+    isProduction
+  },
 
   uploadLimit: process.env.UPLOAD_LIMIT || 15 * 1024 * 1024, // 15MB
 
   jwt: {
-    exp: 14 * 24 * (60 * 60 * 1000),
-    secret: process.env.SESSION_SECRET || 'secretdoggo'
+    exp: 14 * 24 * (60 * 60 * 1000), // 14 days 
+    secret: process.env.SESSION_SECRET || 'secretdoggo',
+    cookie: 'token',
+    cookieOptions: {
+      httpOnly: true,
+      secure: !isDevelopment()
+    }
   },
 
-  test: false,
+  images: {
+    imagesPath: path.join(rootPath, '../../', 'gulp-images/images/'),
+    uploadsPath: path.join(rootPath, '../../', 'gulp-images/uploads/'),
+    uploadsTempPath: path.join(rootPath, '../../', 'gulp-images/uploads-temp/')
+  },
 
   db: {
     uri: process.env.MONGO_URI || 'mongodb://localhost:27017/ericPortfolio',
@@ -65,3 +79,11 @@ module.exports = {
     }
   }
 };
+
+function isDevelopment() {
+  return mode === 'development'
+}
+
+function isProduction() {
+  return mode === 'production'
+}
